5d30dd8b36d0fc7f3f86ed67e83f2d0105e24a35
Avi Perl <avi@aviperl.me>
2025-08-24 03:34:45 -0400

    Revert "github"
    This reverts commit 99ddbdaf8cbd87f4aec6c83cfeb9221dcca8fba0.

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,6 @@
 const nextConfig = {
     experimental: {
         optimizePackageImports: ["@chakra-ui/react"],
-        basePath: '/inventory',
-        assetPrefix: '/inventory/',
     },
     output: 'export',
     basePath: process.env.PAGES_BASE_PATH,
