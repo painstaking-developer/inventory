d17fd92698c00b3b6da5f2d219bd772b0278b163
Avi Perl <avi@aviperl.me>
2025-08-24 04:09:37 -0400

    vercel

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
         <Box as="header" py={1} borderBottomWidth="1px">
             <Container maxW={{base: "100%", md: "1000px"}} px={{base: 4, md: 6}}>
                 <Flex justify="space-between" align="center">
-                    <Box fontWeight="bold">Markdown Forms</Box>
+                    <Box fontWeight="bold">Recovery Inventories</Box>
                     <ColorModeButton/>
                 </Flex>
             </Container>
