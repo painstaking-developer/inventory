99ddbdaf8cbd87f4aec6c83cfeb9221dcca8fba0
Avi Perl <avi@aviperl.me>
2025-08-24 03:07:36 -0400

    github

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
     experimental: {
         optimizePackageImports: ["@chakra-ui/react"],
+        basePath: '/inventory',
+        assetPrefix: '/inventory/',
     },
     output: 'export',
     basePath: process.env.PAGES_BASE_PATH,
