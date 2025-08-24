71b4d3af79a3aab6e8042c2473b5c885c6d77a03
Avi Perl <avi@aviperl.me>
2025-08-24 03:38:28 -0400

    vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,25 +5,23 @@ import {Suspense} from "react";
 
 export default function RootLayout({children}) {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <html lang="en">
-            <body>
-            <Provider>
-                <Box as="main" minH="100vh">
-                    <Header/>
-                    <Container
-                        maxW={{base: "100%", md: "1000px"}}
-                        px={{base: 4, md: 6}}
-                        pt={{base: 4, md: 8}}
-                        pb={{base: 4, md: 8}}
-                        mx="auto"
-                    >
-                        {children}
-                    </Container>
-                </Box>
-            </Provider>
-            </body>
-            </html>
-        </Suspense>
+        <html lang="en">
+        <body>
+        <Provider>
+            <Box as="main" minH="100vh">
+                <Header/>
+                <Container
+                    maxW={{base: "100%", md: "1000px"}}
+                    px={{base: 4, md: 6}}
+                    pt={{base: 4, md: 8}}
+                    pb={{base: 4, md: 8}}
+                    mx="auto"
+                >
+                    {children}
+                </Container>
+            </Box>
+        </Provider>
+        </body>
+        </html>
     );
 }
